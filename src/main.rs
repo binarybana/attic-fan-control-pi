4bbee9c2fac87a4b13a4bb1b6f86f80ee70b8c60
Jason Knight <Jason@JasonKnight.us>
2017-10-15 21:26:14 -0700

    Web controllable gpio, segfaults sometimes though

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,63 @@
-// #![feature(plugin)]
-// #![plugin(rocket_codegen)]
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
-// extern crate rocket;
+extern crate simple_server;
+
+use simple_server::{Server, Method, StatusCode};
+
 extern crate rppal;
 extern crate rand;
-
-// use rocket::State;
-// use std::sync::Mutex;
 
 use rand::Rng;
 
 use rppal::gpio::{GPIO, Mode, Level};
 use rppal::system::DeviceInfo;
 
-use std::thread;
-use std::time::Duration;
-
 // The GPIO module uses BCM pin numbering. BCM 18 equates to physical pin 12.
-const pins: &[u8] = &[15, 18, 23, 24];
-
-// #[get("/high")]
-// fn high(gpio: State<Mutex<GPIO>>) -> &'static str {
-//     let gpio = gpio.lock().unwrap();
-//     gpio.write(GPIO_PIN, Level::High);
-//     "High"
-// }
-//
-// #[get("/low")]
-// fn low(gpio: State<Mutex<GPIO>>) -> &'static str {
-//     let gpio = gpio.lock().unwrap();
-//     gpio.write(GPIO_PIN, Level::Low);
-//     "Low"
-// }
+const PINS: &[u8] = &[15, 18, 23, 24];
 
 fn main() {
     let device_info = DeviceInfo::new().unwrap();
     println!("Model: {} (SoC: {})", device_info.model(), device_info.soc());
 
     let mut gpio = GPIO::new().unwrap();
-    for pin in pins {
+    for pin in PINS {
         gpio.set_mode(*pin, Mode::Output);
     }
-    let mut rng = rand::thread_rng();
 
-    loop {
-        let num = rng.gen::<u8>() & (16-1);
-        println!("\nnum: {:b}", num);
-        for i in 0..4 {
-            print!("writing pin {}", i);
-            match 1 & (num>>i) {
-                0 => gpio.write(pins[i], Level::High),
-                1 => gpio.write(pins[i], Level::Low),
-                _ => panic!("Yo!"),
+    let host = "0.0.0.0";
+    let port = "8000";
+
+    let server = Server::new(|request, mut response| {
+        info!("Request received. {} {}", request.method(), request.uri());
+
+        match (request.method(), request.uri().path()) {
+            (&Method::GET, "/hello") => {
+                Ok(response.body("<h1>Hi!</h1><p>Hello Rust!</p>".as_bytes())?)
             }
-            println!(" ... done");
+            (&Method::GET, "/pinchange") => {
+                let gpio = GPIO::new().unwrap();
+                let mut rng = rand::thread_rng();
+                let num = rng.gen::<u8>() & (16-1);
+                println!("\nnum: {:b}", num);
+                for i in 0..4 {
+                    print!("writing pin {}", i);
+                    match 1 & (num>>i) {
+                        0 => gpio.write(PINS[i], Level::High),
+                        1 => gpio.write(PINS[i], Level::Low),
+                        _ => panic!("Yo!"),
+                    }
+                    println!(" ... done");
+                }
+                Ok(response.body("<h1>Hi!</h1><p>Pins changed!</p>".as_bytes())?)
+            }
+            (_, _) => {
+                response.status(StatusCode::NOT_FOUND);
+                Ok(response.body("<h1>404</h1><p>Not found!<p>".as_bytes())?)
+            }
         }
-        thread::sleep(Duration::from_millis(800));
-    }
+    });
 
-    // rocket::ignite().manage(Mutex::new(gpio)).mount("/", routes![low, high]).launch();
+    server.listen(host, port);
 }
