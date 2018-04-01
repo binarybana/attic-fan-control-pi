565c8c97be87cefe555a6709a6a685b467a2d1a3
Jason Knight <Jason@JasonKnight.us>
2018-03-31 20:58:29 -0700

    Switch to Rust's package manager
    Usage:
        cargo <command> [<args>...]
        cargo [options]
    
    Options:
        -h, --help          Display this message
        -V, --version       Print version info and exit
        --list              List installed commands
        --explain CODE      Run `rustc --explain CODE`
        -v, --verbose ...   Use verbose output (-vv very verbose/build.rs output)
        -q, --quiet         No output printed to stdout
        --color WHEN        Coloring: auto, always, never
        --frozen            Require Cargo.lock and cache are up to date
        --locked            Require Cargo.lock is up to date
        -Z FLAG ...         Unstable (nightly-only) flags to Cargo
    
    Some common cargo commands are (see all commands with --list):
        build       Compile the current project
        check       Analyze the current project and report errors, but don't build object files
        clean       Remove the target directory
        doc         Build this project's and its dependencies' documentation
        new         Create a new cargo project
        init        Create a new cargo project in an existing directory
        run         Build and execute src/main.rs
        test        Run the tests
        bench       Run the benchmarks
        update      Update dependencies listed in Cargo.lock
        search      Search registry for crates
        publish     Package and upload this project to the registry
        install     Install a Rust binary
        uninstall   Uninstall a Rust binary
    
    See 'cargo help <command>' for more information on a specific command. build tool for arm build

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --rm -ti -v `pwd`:/proj -v $HOME/.cargo:/root/.cargo -w /proj pi-builder bash -c '$HOME/.cargo/bin/cargo build --release --target=arm-unknown-linux-musleabihf'
+# docker run --rm -ti -v `pwd`:/proj -v $HOME/.cargo:/root/.cargo -w /proj pi-builder bash -c '$HOME/.cargo/bin/cargo build --release --target=arm-unknown-linux-musleabihf'
+# Now replaced with the much easier:
+cross build --target armv7-unknown-linux-gnueabihf
+
+# Requires cargo install cross
